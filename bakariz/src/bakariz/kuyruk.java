643dd9ef2476208c4a0584ab415b359f01170291
AhmetGH <82283421+AhmetGH@users.noreply.github.com>
2022-12-20 21:44:58 +0300

    Add files via upload

---
+++ bakariz/src/bakariz/kuyruk.java
@@ -0,0 +1,24 @@
+package bakariz;
+
+public class kuyruk {
+
+	public void realtime(){
+		
+	}
+	public void priority_1(){
+		
+	}
+	public void priority_2(){
+		
+	}
+	public void priority_3(){
+	
+	}
+
+	kuyrukCheck()
+	{
+		
+		//
+		
+	}
+}
